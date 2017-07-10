4817b66c60bfad37430fc9fe33fba5b291c45e9c
Lawal Lanre E <larrstonegroup@gmail.com>
2017-07-10 22:52:45 +0100

    Setup database and models
    - create database postit-dev
    - generate models, migrations and seeders folder
    - create user model with username, email, password and phone number fields

---
+++ server/models/user.js
@@ -0,0 +1,21 @@
+module.exports = (sequelize, DataTypes) => {
+  const user = sequelize.define('user', {
+    username: { type: DataTypes.STRING,
+      allowNull: false },
+    email: { type: DataTypes.STRING,
+      allowNull: false },
+    password: { type: DataTypes.STRING,
+      allowNull: false },
+    phone: DataTypes.INTEGER
+  }, {
+    classMethods: {
+      associate: (models) => {
+        models.hasMany(models.Groups, {
+          foreignKey: 'userId',
+          as: 'groups',
+        });
+      }
+    }
+  });
+  return user;
+};
