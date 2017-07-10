316064a732c8d90f8e18f5eee8c28e2ee875c6ca
Lawal Lanre E <larrstonegroup@gmail.com>
2017-07-10 12:42:47 +0100

    setup catch-all ('/') route

--- server/generated/routes/index.js
+++ server/generated/routes/index.js
@@ -1,2 +1,16 @@
-"use strict";
+'use strict';
+
+var _express = require('express');
+
+var _express2 = _interopRequireDefault(_express);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var routes = _express2.default.Router();
+
+routes.get('/', function (req, res) {
+  res.status(200).json({ message: 'Connected!' });
+});
+
+module.exports = routes;
 //# sourceMappingURL=index.js.map
--- server/server/app.js
+++ server/server/app.js
@@ -1,8 +1,20 @@
-import app from 'express';
+import express from 'express';
+import logger from 'morgan';
+import bodyParser from 'body-parser';
+
 import routes from './routes';
 
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended : false}));
+
+//  Connect all our routes
 app.use('/', routes);
 
-app.listen(3000, () => {
-    console.log('App is listening on port 3000');
+// Turn on that server!
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`);
 });
